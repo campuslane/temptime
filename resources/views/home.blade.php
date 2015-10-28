b31663a4db0e37b82c92150ef326c14787320786
frankmatull <frankm@campuslane.com>
2015-10-27 21:01:41 -0700

    initial commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,53 @@
+@extends('layouts.' . $layout)
+
+@section('title', 'TempTime')
+
+@section('content')
+
+	<div class="container">
+		<div class="row">
+			<div class="col-lg-8 col-md-8 col-sm-8">
+				<strong>Monroe Personnel Service, LLC and Temptime</strong> has set a new standard for staffing assistance in the Bay Area. Our clients enjoy unparalleled service, hand-picked candidates and a commitment to long-term partnerships. <br><br>
+
+		An industry leader, our employment agency has built a 26-year reputation rooted in our pledge of integrity and the kind of personalized service only a boutique staffing firm can provide. <br><br>
+
+		Whether your company is growing and you need the highest caliber employees, or you are a busy firm in need of temporary help in a crunch, give us a call! <br><br>
+
+		Our direct-hire consultants are continuously meeting with the absolute top candidates from San Francisco, Oakland, Emeryville, Berkeley and the greater Bay Area. Our temporary consultants have dozens of screened and qualified temps at the ready at all times. 
+			</div>
+
+			<div class="col-lg-4 col-md-4 col-sm-4">
+				<div class="well staffing-solutions" >
+				<h4><i class="fa fa-user " style="color:#ccc"></i> Staffing Solutions</h4>
+
+					<ul class="fa-ul">
+						<li><i class="fa-li fa fa-chevron-right"></i>Administration</li>
+						<li><i class="fa-li fa fa-chevron-right"></i>Accounting</li>
+						<li><i class="fa-li fa fa-chevron-right"></i>Clerical</li>
+						<li><i class="fa-li fa fa-chevron-right"></i>Desktop Support</li>
+						<li><i class="fa-li fa fa-chevron-right"></i>Event Assistance</li>
+						<li><i class="fa-li fa fa-chevron-right"></i>Executive Assistant</li>
+						<li><i class="fa-li fa fa-chevron-right"></i>Human Resources</li>
+						<li><i class="fa-li fa fa-chevron-right"></i>Marketing</li>
+						<li><i class="fa-li fa fa-chevron-right"></i>Reception</li>
+						<li><i class="fa-li fa fa-chevron-right"></i>Telemarketing</li>
+						<li><i class="fa-li fa fa-chevron-right"></i>Corporate</li>
+						<li><i class="fa-li fa fa-chevron-right"></i>Creative</li>
+					</ul>
+				</div>
+			</div>
+		</div>
+
+		<hr>
+
+		<div style="text-align:center;">
+		<img src="/images/convention_logo.jpg" height="100" alt="" style="margin-right:40px;">
+		<img src="/images/sf_chamber_commerce_bw.jpg" height="120" alt="" style="margin-right:40px;">
+		<img src="/images/green_business_bw.jpg" height="100" alt="" style="margin-right:40px;">
+		<img src="/images/sfloma_bw.jpg" height="100" alt="" style="margin-right:40px;">
+		</div>
+
+		<hr>
+	</div>
+
+@endsection
---
+++ resources/views/layouts/temptime.blade.php
@@ -0,0 +1,131 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>@yield('title')</title>
+	<link href="/css/theme.css" rel="stylesheet">
+	<link rel="stylesheet" href="/css/flexslider.css" type="text/css">
+	<link href='https://fonts.googleapis.com/css?family=Raleway:400,300,700,800' rel='stylesheet' type='text/css'>
+	<link href='https://fonts.googleapis.com/css?family=Cardo' rel='stylesheet' type='text/css'>
+	<link href='https://fonts.googleapis.com/css?family=Lora:400,400italic,700,700italic' rel='stylesheet' type='text/css'>
+	@yield('styles')
+
+	<style>
+		/*Caption Text*/
+		.flex-caption {
+		    top:30%;
+		    right:10%;
+		    width:40%;
+		    color: white;
+		    font-size: 16px;
+		    line-height: 20px;
+		    
+		    padding:0 20px;
+		    position:absolute;
+		  
+		    text-transform: uppercase;
+		    z-index:1;
+		    padding:20px;
+		    border:2px solid white;
+		    border-radius:4px;
+		    display:block;
+		    background: rgb(237, 198, 99); /* The Fallback */
+   			background: rgba(237, 198, 99, 0.85); 
+
+   			background: rgb(0, 0, 0); /* The Fallback */
+   			background: rgba(0, 0, 0, 0.50);
+		}
+
+		.top-bar {
+		
+			height:20px;
+		}
+
+	</style>
+</head>
+<body>
+	<div class="top-bar main-color">
+		
+	</div>
+	
+	
+	<div class="container">
+		<div class="clearfix">
+			
+			<h1 style="font-family:Lora;" class="pull-left"><i class="fa fa-clock-o main-color" style="font-size:40px;"></i> TempTime</h1>
+			<div class="pull-right" style="padding-top:32px; font-size:20px; font-weight:300; color:#777; font-family:Lora">
+				Monroe Personnel Service, LLC
+			</div>
+		</div>
+
+	</div>
+	<div class="nav-menu">
+		<div class="container">
+			<span class="pull-right" style="font-weight:bold;"><i class="fa fa-phone main-color"></i> (415) 555-1212</span>
+			<div style="font-size:14px;">
+			HOME &nbsp; &nbsp; ABOUT &nbsp; &nbsp; JOB SEEKERS &nbsp; &nbsp; EMPLOYERS &nbsp; &nbsp; ACCOLADES &nbsp; &nbsp; CONTACT US
+			</div>
+		</div>
+	</div>
+	
+	<div class="container">
+			{{-- <div class="header-band">
+		<div class="container">
+			<div class="row">
+				
+
+				<div class="col-lg-6 col-md-6 col-sm-6" style="margin-top:-20px; margin-bottom:-20px;">
+					{{-- <img src="/images/sf1.jpg" class="img-responsive " alt="San Francisco Skyline"> --}}
+				{{-- </div>
+			</div>
+		</div>
+	</div>  --}}
+			<!-- Place somewhere in the <body> of your page -->
+			<div class="flexslider" style="margin-bottom:20px; background:#eee;">
+			  <ul class="slides">
+			    <li>
+			      <img src="/images/sf1.jpg" />
+			      <span class="flex-caption">The Right Staffing Solutions for your Business</span>
+			    </li>
+			    <li>
+			      <img src="/images/smiling.jpg" />
+			      <span class="flex-caption">Place Hold It Image</span>
+			    </li>
+			    <li>
+			      <img src="/images/woman-standing.jpg" />
+			      <span class="flex-caption">Place Hold It Image</span>
+			    </li>
+			  </ul>
+			</div>
+
+		</div>
+	
+
+	
+
+	@yield('content')
+
+	<br>
+	<br>
+	<br>
+
+
+
+	<script src="/js/app.js"></script>
+	<script src="/js/jquery.flexslider-min.js"></script>
+
+	<!-- Place in the <head>, after the three links -->
+	<script type="text/javascript" charset="utf-8">
+	  $(window).load(function() {
+	    $('.flexslider').flexslider({
+	    	controlNav: false, 
+	    	directionNav: false, 
+	    	animationSpeed: 2000
+	    });
+	  });
+	</script>
+
+	@yield('scripts')
+
+</body>
+</html>
